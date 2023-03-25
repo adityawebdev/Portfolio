c1432a227847cc1cda916f9049b9b2ae00090fe5
Aditya Sinha <adityasinha3201@gmail.com>
2023-03-25 19:05:40 +0530

    passes implemented

--- Desktop/srijan-main/components/Banner.js
+++ Desktop/srijan-main/components/Banner.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 
-import { banner1, banner2, banner3 } from "../assets";
+// IMP --- banner3 keeps breaking, i.e. undefined
+import { banner1, banner2 } from "../assets";
 import Image from "next/image";
 
 const Banner = () => {
-  return <div>banner</div>;
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect} fade>
+        <Carousel.Item interval={300}>
+          <Image
+            src={banner1}
+            className="d-block w-100"
+            style={{ objectFit: "cover" }}
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={300}>
+          <Image
+            src={banner2}
+            className="d-block w-100"
+            style={{ objectFit: "cover" }}
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
